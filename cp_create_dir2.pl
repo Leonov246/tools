5f460463d17b2f8b38976c330693fd234f1e6e55
leonov <leonov246@gmail.com>
2019-07-13 10:35:57 -0700

    Variant of cp_create_dir.pl added

---
+++ cp_create_dir2.pl
@@ -0,0 +1,53 @@
+#!/usr/bin/env perl
+use File::Basename;
+use File::Copy;
+use File::Path;
+use strict;
+use warnings;
+use 5.010;
+use Getopt::Long;
+Getopt::Long::Configure(qw{no_auto_abbrev no_ignore_case_always});
+
+my $verbose;
+my $test;
+my $destname;
+my $destpath;
+
+GetOptions( 'verbose|v' => \$verbose,
+            'test|t' => \$test );
+
+$#ARGV > 0 or die "Not enough arguments\n";
+
+my $sourcefile=$ARGV[0];
+my $destfile=$ARGV[1];
+
+-e $sourcefile or die "$sourcefile does not exist. Aborting\n";
+if($verbose or $test){
+    print "$sourcefile => $destfile\n";
+}
+
+($destname,$destpath) = fileparse($destfile);
+
+if( $test ) {
+    print "Running copy test.\n";
+}
+
+if( not -e $destpath ){
+    if( $test ){
+        print "mkdir $destpath\n";
+    } else {
+        mkpath($destpath);
+    }
+} elsif (not -d $destpath) {
+    die "$destpath is not a directory. Copy aborted.\n";
+}
+
+
+if ($test) {
+    print( "cp $sourcefile $destfile\n");
+} else {
+    copy($sourcefile, $destfile);
+}
+
+exit 0;
+
